d87225323af4499dc6340d365e5ca737837ec4fa
Simon Cockell <sjcockell@gmail.com>
2014-05-06 21:16:52 +0100

    this won't work

--- notifier/notifier.go
+++ notifier/notifier.go
@@ -1,14 +1,27 @@
 package notifier
 
-import ("appengine"
-	"appengine/xmpp")
+import (
+	"fmt"
+	"github.com/mattn/go-xmpp"
+	"log"
+)
 
-func Send(from string, to []string, text string, messageType string) {
-	m := &xmpp.Message{
-		From: from,
-		To:   to,
-		Body: text,
-		Type: messageType,
+
+func Send(server string, username string, password string,
+		notls bool, debug bool, session bool) {
+
+	var talk *xmpp.Client
+	var err error
+	options := xmpp.Options{Host: *server,
+		User:     *username,
+		Password: *password,
+		NoTLS:    *notls,
+		Debug:    *debug,
+		Session:  *session}
+
+	talk, err = options.NewClient()
+
+	if err != nil {
+		log.Fatal(err)
 	}
-	err := m.Send(c)
 }
