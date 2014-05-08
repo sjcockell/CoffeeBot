55b87f5ce6f8dfafeae3dc2ac2de5e787e83b126
Simon Cockell <sjcockell@gmail.com>
2014-05-08 22:32:55 +0100

    New library test

--- notifier/notifier.go
+++ notifier/notifier.go
@@ -3,11 +3,14 @@ package notifier
 import ("code.google.com/p/goexmpp")
 
 func Send(from string, to []string, text string, messageType string) {
-	m := &xmpp.Message{
-		From: from,
-		To:   to,
-		Body: text,
-		Type: messageType,
-	}
-	err := m.Send(c)
+	var jid xmpp.JID = from
+  var pw string = "notapassword"
+  if jid.Domain == "" || *pw == "" {
+    os.Exit(2)
+  }
+  c, err := xmpp.NewClient(&jid, *pw, nil)
+  if err != nil {
+    log.Fatalf("NewClient(%v): %v", jid, err)
+  }
+  defer close(c.Out)
 }
